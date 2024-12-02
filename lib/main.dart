2c225637f646fe79e3c8393223b6599a0d0c6371
DelfinAngelo <delfinangelo.lamasan@wvsu.edu.ph>
2024-12-02 22:06:51 +0800

    Add-ons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
-// File: lib/main.dart
-Navigator.push(
-  context,
-  MaterialPageRoute(
-    builder: (context) => CompletedTasksScreen(completedTasks: _completedTasks),
-  ),
-);
+void _loadTasks() async {
+  final prefs = await SharedPreferences.getInstance();
+  setState(() {
+    _tasks.addAll(prefs.getStringList('tasks') ?? []);
+    _completedTasks.addAll(prefs.getStringList('completedTasks') ?? []);
+  });
+}
+
+void _saveTasks() async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setStringList('tasks', _tasks);
+  prefs.setStringList('completedTasks', _completedTasks);
+}
